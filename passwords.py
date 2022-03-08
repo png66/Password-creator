55c0e18575915fbae3836b074f5689e5a8569c7a
png <vaniasarvirov@gmail.com>
2022-03-08 16:20:11 +0300

    1.1st commit

--- passwords.py
+++ passwords.py
@@ -1,7 +1,7 @@
 import random
-
+print("Это программа для создания паролей")
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-length = int(input('Введите длину пароля')) 
+length = int(input('Введите длину пароля:')) 
 password =''
 if length >= 12:
     for i in range(length):
